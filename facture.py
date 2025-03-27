4f9a2bad2da79987843a885823fa0baa31bce583
zakaria2122 <zakaria.makhlouf45000@gmail.com>
2025-03-27 11:43:06 +0100

    elif

--- facture.py
+++ facture.py
@@ -2,16 +2,17 @@ import sqlalchemy
 import argparse
 import getpass
 
+from sqlalchemy import text
+
 class MySQL(object):
-    def __init__(self, user, passwd, host, database,timeout=20):
+    def __init__(self, user, passwd, host, database, timeout=20):
         self.user = user
         self.passwd = passwd
         self.host = host
         self.database = database
-        #try:
         self.engine = sqlalchemy.create_engine(
-                'mysql+mysqlconnector://' + self.user + ':' + self.passwd + '@' + self.host + '/' + self.database,
-                )
+            f'mysql+mysqlconnector://{self.user}:{self.passwd}@{self.host}/{self.database}',
+        )
         self.cnx = self.engine.connect()
         print("connexion réussie")
 
@@ -19,50 +20,154 @@ class MySQL(object):
         self.cnx.close()
 
     def execute(self, requete, liste_parametres):
+        # Remplacer les "?" par les paramètres dans la requête
         for param in liste_parametres:
-            if type(param)==str:
-                requete=requete.replace('?',"'"+param+"'",1)
+            if isinstance(param, str):
+                requete = requete.replace('?', f"'{param}'", 1)
             else:
-                requete=requete.replace('?',str(param),1)
+                requete = requete.replace('?', str(param), 1)
+        # Exécuter la requête modifiée
+        requete = text(requete)
         return self.cnx.execute(requete)
 
-def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
-    # exécute la requête en remplaçant le premier ? par le numéro du mois 
-    # et le deuxième ? par l'année
-    curseur=bd.execute(requete,(mois,annee))
-    # Initialisations du traitement
-    res='pas encore implémentée'
-    for ligne in curseur:
-        # parcours du résultat de la requête. 
-        # ligne peut être vu comme un dictionnaire dont les clés sont les noms des colonnes de votre requête
-        # est les valeurs sont les valeurs de ces colonnes pour la ligne courante
-        # par exemple ligne['numcom'] va donner le numéro de la commande de la ligne courante 
-        ...
+    
 
-    #ici fin du traitement
-    # fermeture de la requête
+
+
+def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
+
+    curseur=bd.execute(requete,(mois,annee))
+
+    # Initialisation des variables
+    res = []
+    chiffre_affaire_global = 0
+    total_livres_vendus_global = 0
+    nombre_factures_global = 0
+    magasin_actuel = None
+    facture_courante = None
+    total_commande = 0
+    total_livres_vendus_magasin = 0
+    nombre_factures_magasin = 0
+
+    res = []
+    chiffre_affaire_global = 0
+    total_livres_vendus_global = 0
+    nombre_factures_global = 0
+    magasin_actuel = None
+    facture_courante = None
+    total_commande = 0
+    total_livres_vendus_magasin = 0
+    nombre_factures_magasin = 0
+
+    res.append(f"Factures du {mois}/{annee}")
+    
+    for ligne in curseur:
+        magasin = ligne[0]
+        client_nom = ligne[1]
+        client_prenom = ligne[2]
+        client_adresse = f"{ligne[3]}\n{ligne[4]} {ligne[5]}"
+        numcom = ligne[6]
+        datecom = ligne[7].strftime("%d/%m/%Y")
+        isbn = ligne[8]
+        titre = ligne[9]
+        quantite = ligne[10]
+        prix_unitaire = ligne[11]
+        prix_total = ligne[12]
+
+        # Accumulation correcte des totaux
+        chiffre_affaire_global += prix_total
+        total_livres_vendus_global += quantite
+
+        # Si on change de magasin, on affiche le résumé du précédent et on réinitialise les compteurs
+        if magasin_actuel != magasin:
+
+            res.append(f"\nEdition des factures du magasin {magasin}")
+            res.append(f"{'-' * 85}")
+            magasin_actuel = magasin
+            nombre_factures_magasin = 0
+            total_livres_vendus_magasin = 0
+        elif  magasin_actuel is not None:
+                res.append(f"\n{'-' * 85}")
+                res.append(f"Résumé du magasin {magasin_actuel} :")
+                res.append(f"  - {nombre_factures_magasin} factures éditées")
+                res.append(f"  - {total_livres_vendus_magasin} livres vendus")
+                res.append(f"{'-' * 85}")
+
+        # Si on change de commande, on affiche la facture précédente et on en commence une nouvelle
+        if facture_courante != numcom:
+            res.append(f"\n{client_nom} {client_prenom}")
+            res.append(f"{client_adresse}")
+            res.append(f"Commande n°{numcom} du {datecom}")
+            res.append(f"{'ISBN':<10} {'Titre':<40} {'Qte':<6} {'Prix Unitaire':<15} {'Total':>10}")
+            res.append(f"{'-' * 85}")
+            facture_courante = numcom
+            nombre_factures_magasin += 1
+            nombre_factures_global += 1
+            total_commande = 0 
+        elif  facture_courante is not None:
+                res.append(f"\n{'-' * 85}")
+                res.append(f"Total de la commande : {total_commande:.2f} €")
+                res.append(f"{'-' * 85}") 
+
+        # Ajout de la ligne de commande
+        res.append(f"{isbn:<10} {titre[:40]:<40} {quantite:<6} {prix_unitaire:<15.2f} {prix_total:<15.2f}")
+        total_commande += prix_total
+        total_livres_vendus_magasin += quantite
+
+    # Finalisation de la dernière facture
+    if facture_courante is not None:
+        res.append(f"\n{'-' * 85}")
+        res.append(f"Total de la commande : {total_commande:.2f} €")
+        res.append(f"{'-' * 85}")
+
+    # Ajout du résumé du dernier magasin
+    if magasin_actuel is not None:
+        res.append(f"\nRésumé du magasin {magasin_actuel} :")
+        res.append(f"  - {nombre_factures_magasin} factures éditées")
+        res.append(f"  - {total_livres_vendus_magasin} livres vendus")
+        res.append(f"{'-' * 85}")
+
+    # Ajout du résumé global
+    res.append(f"\n{'*' * 85}")
+    res.append(f"Chiffre d’affaire global: {chiffre_affaire_global:.2f} €")
+    res.append(f"Nombre de livres vendus: {total_livres_vendus_global}")
+    res.append(f"{'*' * 85}")
+
+    # Fermeture du curseur et retour du résultat
     curseur.close()
-    return res
-        
+    return "\n".join(res)
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--serveur",dest="nomServeur", help="Nom ou adresse du serveur de base de données", type=str, default="127.0.0.1")
+    parser.add_argument("--serveur", dest="nomServeur", help="Nom ou adresse du serveur de base de données", type=str, default="servinfo-maria")
+
     parser.add_argument("--bd",dest="nomBaseDeDonnees", help="Nom de la base de données", type=str,default='Librairie')
-    parser.add_argument("--login",dest="nomLogin", help="Nom de login sur le serveur de base de donnée", type=str, default='limet')
-    parser.add_argument("--requete", dest="fichierRequete", help="Fichier contenant la requete des commandes", type=str)    
+    parser.add_argument("--login",dest="nomLogin", help="Nom de login sur le serveur de base de donnée", type=str, default='makhlouf')
+    parser.add_argument("--requete", dest="fichierRequete", help="Fichier contenant la requete des commandes", type=str, default='fic_req.sql')  
     args = parser.parse_args()
-    passwd = getpass.getpass("mot de passe SQL:")
+    
+    # Demande de mot de passe
+    passwd = getpass.getpass("Mot de passe SQL : ")
+    
     try:
         ms = MySQL(args.nomLogin, passwd, args.nomServeur, args.nomBaseDeDonnees)
     except Exception as e:
-        print("La connection a échoué avec l'erreur suivante:", e)
+        print("La connexion a échoué avec l'erreur suivante:", e)
         exit(0)
-    rep=input("Entrez le mois et l'année sous la forme mm/aaaa ")
-    mm,aaaa=rep.split('/')
-    mois=int(mm)
-    annee=int(aaaa)
+    
+    # Demander à l'utilisateur d'entrer le mois et l'année
+    rep = input("Entrez le mois et l'année sous la forme mm/aaaa : ")
+    mm, aaaa = rep.split('/')
+
+    mois = int(mm)
+    annee = int(aaaa)
+    
+    # Lire la requête depuis le fichier
     with open(args.fichierRequete) as fic_req:
-        requete=fic_req.read()
-    print(faire_factures(requete,mois,annee,ms))
+        requete = fic_req.read()
+    
+    # Afficher le résultat de la fonction
+    print(faire_factures(requete, mois, annee, ms))
+    ms.close()
--- sae2025.sql
+++ sae2025.sql
@@ -1,16 +1,19 @@
 -- Devoir 127
--- Nom: CHER , Prenom: Naick
+-- Nom: MOINS, MAKHLOUF , Prenom: Bastien
+
 
 -- Feuille SAE2.05 Exploitation d'une base de données: Livre Express
--- 
+--
 -- Veillez à bien répondre aux emplacements indiqués.
 -- Seule la première requête est prise en compte.
+
 
 -- +-----------------------+--
 -- * Question 127156 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Quels sont les livres qui ont été commandés le 1er décembre 2024 ?
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -20,11 +23,16 @@
 -- | etc...
 -- = Reponse question 127156.
 
-select distinct  isbn, titre, nbpages, datepubli, prix 
+
+select distinct  isbn, titre, nbpages, datepubli, prix
 from CLIENT natural join COMMANDE natural join LIVRE natural join AUTEUR natural join ECRIRE natural join DETAILCOMMANDE
+
 
 where datecom = '2024-12-01'
 order by titre;
+
+
+
 
 
 
@@ -34,6 +42,7 @@ order by titre;
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Quels clients ont commandé des livres de René Goscinny en 2021 ?
 
+
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +-------+---------+-----------+-----------------------------+------------+-------------+
@@ -41,16 +50,18 @@ order by titre;
 -- +-------+---------+-----------+-----------------------------+------------+-------------+
 -- | etc...
 -- = Reponse question 127202.
-select distinct idcli, nomcli, prenomcli, adressecli, codepostal, villecli  
+select distinct idcli, nomcli, prenomcli, adressecli, codepostal, villecli 
 from CLIENT natural join COMMANDE natural join LIVRE natural join AUTEUR natural join ECRIRE natural join DETAILCOMMANDE
 where YEAR(dateCom)=2021 and nomauteur = 'René Goscinny'
 order by nomcli, prenomcli;
+
 
 -- +-----------------------+--
 -- * Question 127235 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Quels sont les livres sans auteur et étant en stock dans au moins un magasin en quantité strictement supérieure à 8 ?
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -62,10 +73,13 @@ order by nomcli, prenomcli;
 select distinct isbn, titre, nommag, qte
 from POSSEDER natural join MAGASIN natural join LIVRE
 where isbn not in (
-    select isbn
-    from ECRIRE
+   select isbn
+   from ECRIRE
 ) and qte > 8
 order by titre, nommag;
+
+
+
 
 
 
@@ -75,6 +89,7 @@ order by titre, nommag;
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Pour chaque magasin, on veut le nombre de clients qui habitent dans la ville de ce magasin (en affichant les 0)
 
+
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +-------+-------------------------+-------+
@@ -82,11 +97,14 @@ order by titre, nommag;
 -- +-------+-------------------------+-------+
 -- | etc...
 -- = Reponse question 127279.
-SELECT m.idmag, m.nommag, COUNT(DISTINCT c.idcli) AS nbcli
-FROM MAGASIN m
-LEFT JOIN COMMANDE co ON m.idmag = co.idmag
-LEFT JOIN CLIENT c ON co.idcli = c.idcli AND c.villecli = m.villemag
-GROUP BY m.idmag, m.nommag;
+
+select idmag, nommag, IFNULL(COUNT(DISTINCT idcli),0) AS nbcli
+from MAGASIN 
+natural left join COMMANDE natural left join CLIENT 
+where CLIENT.villecli = MAGASIN.villemag
+group by idmag, nommag;
+
+
 
 
 
@@ -95,6 +113,7 @@ GROUP BY m.idmag, m.nommag;
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Pour chaque magasin, on veut la quantité de livres achetés le 15/09/2022 en affichant les 0.
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -110,11 +129,15 @@ group by nommag;
 
 
 
+
+
+
 -- +-----------------------+--
 -- * Question 127314 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Instructions d'insertion dans la base de données
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -125,16 +148,20 @@ group by nommag;
 -- = Reponse question 127314.
 
 
+
+
 insert into LIVRE values ('9782844273765', 'SQL pour les Nuls', 292, 2002, 33.5);
 insert into AUTEUR values ('OL246259A', 'Taylor Allen G.', NULL, NULL);
 insert into AUTEUR values ('OL7670824A', 'Engel Reinhard', NULL, NULL);
 insert into POSSEDER values (7, '9782844273765', 3);
+
 
 -- +-----------------------+--
 -- * Question 127369 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 1 Nombre de livres vendus par magasin et par an
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -143,6 +170,11 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-------------------------+-------+-----+
 -- | etc...
 -- = Reponse question 127369.
+select distinct nommag as Magasin,year(datecom) as Année, sum(qte)
+from MAGASIN natural join COMMANDE natural join DETAILCOMMANDE
+group by Magasin, Année
+order by Magasin, Année;
+
 
 
 
@@ -152,6 +184,7 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 2  Chiffre d'affaire par thème en 2024
 
+
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +--------------------------------------+---------+
@@ -159,7 +192,22 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +--------------------------------------+---------+
 -- | etc...
 -- = Reponse question 127370.
+--https://sql.sh/cours/case
 
+
+     SELECT  case      WHEN LEFT(iddewey, 1) = '0' THEN 'Informatique, généralités'
+       WHEN LEFT(iddewey, 1) = '1' THEN 'Philosophie et psychologie'
+       WHEN LEFT(iddewey, 1) = '2' THEN 'Religion'
+       WHEN LEFT(iddewey, 1) = '3' THEN 'Sciences sociales'
+       WHEN LEFT(iddewey, 1) = '4' THEN 'Langues'
+       WHEN LEFT(iddewey, 1) = '5' THEN 'Sciences naturelles et mathématiques'
+       WHEN LEFT(iddewey, 1) = '6' THEN 'Technologie et sciences appliquées'
+       WHEN LEFT(iddewey, 1) = '7' THEN 'Arts et loisirs'
+       WHEN LEFT(iddewey, 1) = '8' THEN 'Littérature'
+       WHEN LEFT(iddewey, 1) = '9' THEN 'Histoire et géographie'end AS Theme, SUM(prixvente * qte) AS Montant
+FROM CLASSIFICATION NATURAL  JOIN THEMES NATURAL  JOIN LIVRE natural  join DETAILCOMMANDE NATURAL JOIN COMMANDE
+WHERE YEAR(datecom) = '2024' 
+GROUP BY theme ;
 
 
 -- +-----------------------+--
@@ -168,6 +216,7 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 3 Evolution chiffre d'affaire par magasin et par mois en 2024
 
+
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +------+-------------------------+---------+
@@ -175,6 +224,11 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +------+-------------------------+---------+
 -- | etc...
 -- = Reponse question 127381.
+select distinct MONTH(datecom) as mois, nommag as Magasin, sum(prixvente*qte) as CA
+from MAGASIN natural join COMMANDE natural join DETAILCOMMANDE
+where YEAR(datecom) = '2024'
+group by Magasin, mois;
+
 
 
 
@@ -183,6 +237,7 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 4 Comparaison ventes en ligne et ventes en magasin
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -193,12 +248,21 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- = Reponse question 127437.
 
 
+select YEAR(datecom) as annee, case
+when enligne = 'O' then 'En ligne'
+else 'En magasin'
+end as
+ typevente, sum(prixvente * qte) as montant
+from COMMANDE natural join DETAILCOMMANDE
+group by annee, typevente;
+
 
 -- +-----------------------+--
 -- * Question 127471 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 5
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -207,7 +271,11 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-------------------+-----------+
 -- | etc...
 -- = Reponse question 127471.
-
+select distinct nomedit as Editeur, count(idauteur) as nbauteurs
+from EDITEUR natural join EDITER natural join LIVRE natural join ECRIRE natural join AUTEUR
+group by nomedit
+order by nbauteurs desc
+limit 10;
 
 
 -- +-----------------------+--
@@ -216,6 +284,7 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 6 Origine des clients ayant acheter des livres de R. Goscinny
 
+
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +-------------+-----+
@@ -223,6 +292,11 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-------------+-----+
 -- | etc...
 -- = Reponse question 127516.
+select distinct villecli as ville, sum(qte) as qte
+from CLIENT natural join COMMANDE natural join LIVRE natural join AUTEUR natural join ECRIRE natural join DETAILCOMMANDE
+where nomauteur = 'René Goscinny'
+group by ville;
+
 
 
 
@@ -231,8 +305,6 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
 --  Requête Graphique 7 Valeur du stock par magasin
---  Requête Graphique 8 Statistiques sur l'évolution du chiffre d'affaire total par client 
-
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +-------------------------+---------+
@@ -240,15 +312,16 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-------------------------+---------+
 -- | etc...
 -- = Reponse question 127527.
-
+select distinct nommag as Magasin, sum(prix*qte) as total
+from MAGASIN natural join POSSEDER natural join LIVRE
+group by Magasin;
 
 
 -- +-----------------------+--
 -- * Question 127538 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
---  Requête Palmarès
-
+-- Requête Graphique 8 Statistiques sur l'évolution du chiffre d'affaire total par client
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
 -- +-------+---------+---------+---------+
@@ -259,11 +332,24 @@ insert into POSSEDER values (7, '9782844273765', 3);
 
 
 
+
+with TotalCa as (
+   select YEAR(datecom) as annee, sum(prixvente*qte) as total
+   from CLIENT natural join COMMANDE natural join DETAILCOMMANDE
+   group by idcli, annee
+)
+select distinct idcli, annee, max(total) as maximum, min(total) as minimum, avg(total) as moyenne
+from TotalCa
+group by idcli, annee
+order by annee;
+
+
 -- +-----------------------+--
 -- * Question 127572 : 2pts --
 -- +-----------------------+--
 -- Ecrire une requête qui renvoie les informations suivantes:
---  Requête imprimer les commandes en considérant que l'on veut celles de février 2020
+--  Requête Palmarès
+
 
 -- Voici le début de ce que vous devez obtenir.
 -- ATTENTION à l'ordre des colonnes et leur nom!
@@ -272,6 +358,38 @@ insert into POSSEDER values (7, '9782844273765', 3);
 -- +-------+-----------------------+-------+
 -- | etc...
 -- = Reponse question 127572.
+WITH AnneeTotal AS (
+   SELECT YEAR(datecom) AS annee, nomauteur, SUM(qte) AS total
+   FROM COMMANDE NATURAL JOIN DETAILCOMMANDE NATURAL JOIN LIVRE NATURAL JOIN AUTEUR NATURAL JOIN ECRIRE
+   WHERE YEAR(datecom) != 2025
+   GROUP BY annee, nomauteur
+)
+select distinct at1.annee, at1.nomauteur, at1.total
+from AnneeTotal at1
+where total = (select max(at2.total) from AnneeTotal at2 where at2.annee = at1.annee)
+order by annee asc;
+
+
+-- +-----------------------+--
+-- * Question 127572 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requête qui renvoie les informations suivantes:
+--  Requête imprimer les commandes en considérant que l'on veut celles de février 2020
+
+
+-- Voici le début de ce que vous devez obtenir.
+-- ATTENTION à l'ordre des colonnes et leur nom!
+-- +-------+-----------------------+-------+
+-- | annee | nomauteur             | total |
+-- +-------+-----------------------+-------+
+-- | etc...
+-- = Reponse question 127572.
+select YEAR(datecom) as annee, nomauteur, SUM(qte) as total
+FROM COMMANDE NATURAL JOIN DETAILCOMMANDE NATURAL JOIN LIVRE NATURAL JOIN AUTEUR NATURAL JOIN ECRIRE
+where YEAR(datecom) = 2020 and MONTH(datecom) = 2
+GROUP BY annee, nomauteur
+order by datecom;
+
 
 
 
