2bc98ed0c0ce68d7e75fa3095aa90d7e24935027
Bastien-MOINS <bastien.moins@gmail.com>
2025-03-28 15:29:30 +0100

    modifier

--- Rendu/facture.py
+++ Rendu/facture.py
@@ -33,27 +33,14 @@ class MySQL(object):
 
 
 
-def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
-
-   
-    curseur=bd.execute(requete,(mois,annee))
-
-
-    # Initialisation des variables
-    res = []
-    chiffre_affaire_global = 0
-    total_livres_vendus_global = 0
-    nombre_factures_global = 0
-    magasin_actuel = None
-    facture_courante = None
-    total_commande = 0
-    total_livres_vendus_magasin = 0
-    nombre_factures_magasin = 0
+def faire_factures(requete: str, mois: int, annee: int, bd: MySQL):
+    curseur = bd.execute(requete, (mois, annee))
 
     res = []
     chiffre_affaire_global = 0
     total_livres_vendus_global = 0
     nombre_factures_global = 0
+
     magasin_actuel = None
     facture_courante = None
     total_commande = 0
@@ -61,7 +48,7 @@ def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
     nombre_factures_magasin = 0
 
     res.append(f"Factures du {mois}/{annee}")
-    
+
     for ligne in curseur:
         magasin = ligne[0]
         client_nom = ligne[1]
@@ -75,75 +62,76 @@ def faire_factures(requete:str, mois:int, annee:int, bd:MySQL):
         prix_unitaire = ligne[11]
         prix_total = ligne[12]
 
-        # Accumulation correcte des totaux
         chiffre_affaire_global += prix_total
         total_livres_vendus_global += quantite
 
-        # Si on change de magasin, on affiche le résumé du précédent et on réinitialise les compteurs
+        # Changement de magasin
         if magasin_actuel != magasin:
+            if magasin_actuel is not None:
+                # Ajouter le résumé du magasin précédent
+                res.append(f"{'-' * 85}")
+                res.append(f"{nombre_factures_magasin} factures éditées")
+                res.append(f"{total_livres_vendus_magasin} livres vendus")
+                res.append(f"{'*' * 85}")
 
+            # Nouvelle section pour le magasin
             res.append(f"\nEdition des factures du magasin {magasin}")
             res.append(f"{'-' * 85}")
+
             magasin_actuel = magasin
             nombre_factures_magasin = 0
             total_livres_vendus_magasin = 0
-        elif  magasin_actuel is not None:
-                res.append(f"\n{'-' * 85}")
-                res.append(f"Résumé du magasin {magasin_actuel} :")
-                res.append(f"  - {nombre_factures_magasin} factures éditées")
-                res.append(f"  - {total_livres_vendus_magasin} livres vendus")
+
+        # Changement de facture
+        if facture_courante != numcom:
+            if facture_courante is not None:
+                res.append(f"{'-' * 85}")
+                res.append(f"Total de la commande : {total_commande:.2f} €")
                 res.append(f"{'-' * 85}")
 
-        # Si on change de commande, on affiche la facture précédente et on en commence une nouvelle
-        if facture_courante != numcom:
             res.append(f"\n{client_nom} {client_prenom}")
-            res.append(f"{client_adresse}")
+            res.append(client_adresse)
             res.append(f"Commande n°{numcom} du {datecom}")
-            res.append(f"{'ISBN':<10} {'Titre':<40} {'Qte':<6} {'Prix Unitaire':<15} {'Total':>10}")
+            res.append(f"{'ISBN':<15}{'Titre':<40}{'Qte':<6}{'Prix':<10}{'Total':<10}")
             res.append(f"{'-' * 85}")
+
             facture_courante = numcom
             nombre_factures_magasin += 1
             nombre_factures_global += 1
-            total_commande = 0 
-        elif  facture_courante is not None:
-                res.append(f"\n{'-' * 85}")
-                res.append(f"Total de la commande : {total_commande:.2f} €")
-                res.append(f"{'-' * 85}") 
+            total_commande = 0
 
-        # Ajout de la ligne de commande
-        res.append(f"{isbn:<10} {titre[:40]:<40} {quantite:<6} {prix_unitaire:<15.2f} {prix_total:<15.2f}")
+        # Ajouter la ligne de commande
+        res.append(f"{isbn:<15}{titre[:40]:<40}{quantite:<6}{prix_unitaire:<10.2f}{prix_total:<10.2f}")
         total_commande += prix_total
         total_livres_vendus_magasin += quantite
 
-    # Finalisation de la dernière facture
+    # Finalisation de la dernière commande
     if facture_courante is not None:
-        res.append(f"\n{'-' * 85}")
+        res.append(f"{'-' * 85}")
         res.append(f"Total de la commande : {total_commande:.2f} €")
         res.append(f"{'-' * 85}")
 
-    # Ajout du résumé du dernier magasin
+    # Ajout du dernier résumé de magasin
     if magasin_actuel is not None:
-        res.append(f"\nRésumé du magasin {magasin_actuel} :")
-        res.append(f"  - {nombre_factures_magasin} factures éditées")
-        res.append(f"  - {total_livres_vendus_magasin} livres vendus")
         res.append(f"{'-' * 85}")
+        res.append(f"{nombre_factures_magasin} factures éditées")
+        res.append(f"{total_livres_vendus_magasin} livres vendus")
+        res.append(f"{'*' * 85}")
 
-    # Ajout du résumé global
+    # Résumé global
     res.append(f"\n{'*' * 85}")
     res.append(f"Chiffre d’affaire global: {chiffre_affaire_global:.2f} €")
-    res.append(f"Nombre de livres vendus: {total_livres_vendus_global}")
+    res.append(f"Nombre total de livres vendus: {total_livres_vendus_global}")
     res.append(f"{'*' * 85}")
 
-    # Fermeture du curseur et retour du résultat
     curseur.close()
     return "\n".join(res)
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--serveur",dest="nomServeur", help="Nom ou adresse du serveur de base de données", type=str, default="servinfo-maria")
     parser.add_argument("--bd",dest="nomBaseDeDonnees", help="Nom de la base de données", type=str,default='Librairie')
-    parser.add_argument("--login",dest="nomLogin", help="Nom de login sur le serveur de base de donnée", type=str, default='makhlouf')
+    parser.add_argument("--login",dest="nomLogin", help="Nom de login sur le serveur de base de donnée", type=str, default='moins')
     parser.add_argument("--requete", dest="fichierRequete", help="Fichier contenant la requete des commandes", type=str , default='fic_req.sql')    
     args = parser.parse_args()
     passwd = getpass.getpass("mot de passe SQL:")
